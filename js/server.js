7126d88cf3e40505f5698465b31105f241ae24d1
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-18 18:49:49 +0000

    Cleaned up root directory.

---
+++ js/server.js
@@ -0,0 +1,13 @@
+var server = require('express')();
+var http = require('http').createServer(server);
+
+server.set('views', __dirname + '/views');
+
+server.get('/', function(request, response) {
+  response.render('index')
+});
+
+
+http.listen(3000, function(){
+  console.log('Listening on port 3000');
+});
