a2749cc286f52b998aa061a557541497f0e521e1
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-29 17:03:53 +0200

    Fixed protractor tests.

--- spec/feature/enterRollFeature.spec.js
+++ spec/feature/enterRollFeature.spec.js
@@ -5,12 +5,12 @@ describe('User on Bowling Calculator Page', function(){
 
 
   it('can choose x out of ten pins', function(){
-    expect(element.all(by.css('.pin')).count()).toEqual(10);
+    expect(element.all(by.css('.pin')).count()).toEqual(11);
   });
 
   it('sees only as many pins as there are left in current frame', function(){
-    element.all(by.css('.pin')).first().click();
-    expect(element.all(by.css('.pin')).count()).toEqual(9);
+    element.all(by.binding('pin')).get(1).click();
+    expect(element.all(by.css('.pin')).count()).toEqual(10);
   });
 
   it('can save a roll by clicking a pin number', function(){
@@ -21,8 +21,8 @@ describe('User on Bowling Calculator Page', function(){
 
   it('shows the current score after frame is finished', function(){
     element.all(by.css('.pin')).first().click();
-    element.all(by.css('.pin')).first().click();
-    expect(element(by.id('current-score')).getText()).toEqual('Current Score: 2')
+    element.all(by.css('.pin')).last().click();
+    expect(element(by.id('current-score')).getText()).toEqual('Current Score: 10')
   });
 
 });
