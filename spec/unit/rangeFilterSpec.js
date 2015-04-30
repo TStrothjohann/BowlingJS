859890150a32b985e39a0370bef9795ef723465a
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-30 10:40:09 +0200

    Custom rangeFilter is tested.

---
+++ spec/unit/rangeFilterSpec.js
@@ -0,0 +1,12 @@
+describe('filter', function() {
+
+  beforeEach(module('bowling'));
+
+  describe('rangeFilter', function() {
+
+    it('produces an array with given range',
+        inject(function(rangeFilter) {
+      expect(rangeFilter([], 11)).toEqual([0,1,2,3,4,5,6,7,8,9,10]);
+    }));
+  });
+});
